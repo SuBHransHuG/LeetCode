65f79f4dd9012b0d65f0c70560a307de94513a9e
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-08-06 22:31:22 +0530

    3016. Minimum Number of Pushes to Type Word II

---
+++ Daily-Questions/3016. Minimum Number of Pushes to Type Word II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumPushes(String word) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+        ArrayList<Integer> al = new ArrayList<>(hm.values());
+        Collections.sort(al,Collections.reverseOrder());
+        int tp = 0;
+        for (int i = 0; i < al.size(); i++) {
+           if(al.get(i) == 0) break;
+           tp +=(i/8+1)*al.get(i);
+        }
+        return tp;
+    }
+}
