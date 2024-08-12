e021483fe3fef522092d315ffb45a7f6e5b3aba7
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-08-12 22:45:31 +0530

    Kth Largest Element in a Stream

---
+++ Daily-Questions/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,22 @@
+class KthLargest {
+    private final PriorityQueue<Integer> minHeap;
+    private final int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+        return minHeap.peek();
+    }
+}
